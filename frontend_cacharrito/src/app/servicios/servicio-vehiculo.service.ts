4ad75c23f5661b9ed62645d3b3bdec57adf3834c
Valegg01 <valentinagomezg75@gmail.com>
2025-03-13 16:18:37 -0500

    represntacion de vehiculos con filtro de tipo

--- frontend_cacharrito/src/app/servicios/servicio-vehiculo.service.ts
+++ frontend_cacharrito/src/app/servicios/servicio-vehiculo.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Vehiculo } from '../Entidades/vehiculo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioVehiculoService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  buscar_tipo(Tipo:string):Observable<any>{
+    return this.httpClient.get(`http://localhost:8080/conVehiculo/Tipo?Tipo=${Tipo}`)
+  }
 }
--- frontend_cacharrito/src/app/vehiculos/vehiculos.component.ts
+++ frontend_cacharrito/src/app/vehiculos/vehiculos.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { ServicioVehiculoService } from '../servicios/servicio-vehiculo.service';
+import { FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Vehiculo } from '../Entidades/vehiculo';
 
 @Component({
   selector: 'app-vehiculos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './vehiculos.component.html',
   styleUrl: './vehiculos.component.css'
 })
 export class VehiculosComponent {
+  constructor(private serveh:ServicioVehiculoService){}
+
+  Tipo!: string;
+
+  vehiculo!:Vehiculo[];
+
+  buscar_tipo(){
+
+    this.serveh.buscar_tipo(this.Tipo).subscribe(dato=>{
+
+
+      if(dato ==null){
+
+        alert("no hay datos disponibles")
+
+
+      }else{
+
+        console.log(dato)
+        this.vehiculo=dato;
+
+      }
+    
+
+    },error=>{
+      alert("error al procesar la solicitud")
+    })
+
+  }
 
 }
