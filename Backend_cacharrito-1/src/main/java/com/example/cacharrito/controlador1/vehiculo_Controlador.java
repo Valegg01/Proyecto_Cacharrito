12adfd9cd289ae033364cd7c0dafe2c8fb2b30fe
Valegg01 <valentinagomezg75@gmail.com>
2025-03-13 12:40:10 -0500

    creacion de metodo de filtrol de vehiculos

--- Backend_cacharrito-1/src/main/java/com/example/cacharrito/controlador1/vehiculo_Controlador.java
+++ Backend_cacharrito-1/src/main/java/com/example/cacharrito/controlador1/vehiculo_Controlador.java
@@ -1,5 +1,24 @@
 package com.example.cacharrito.controlador1;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.cacharrito.modelo1.vehiculo;
+import com.example.cacharrito.repositorio1.vehiculo_Repositorio;
+
 public class vehiculo_Controlador {
+	
+	@Autowired
+	
+	private vehiculo_Repositorio RepVehiculo;
+	
+	@PostMapping("/filtro")
+	public List<vehiculo> filtroVheiculo(@RequestParam String filtro){
+		
+		return this.RepVehiculo.findByestado(filtro);
+	}
 
 }
--- Backend_cacharrito-1/src/main/java/com/example/cacharrito/repositorio1/vehiculo_Repositorio.java
+++ Backend_cacharrito-1/src/main/java/com/example/cacharrito/repositorio1/vehiculo_Repositorio.java
@@ -1,9 +1,13 @@
 package com.example.cacharrito.repositorio1;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.cacharrito.modelo1.vehiculo;
 
 public interface vehiculo_Repositorio  extends JpaRepository <vehiculo, Long> {
+	
+	List<vehiculo> findByestado(String estado);
 
 }
