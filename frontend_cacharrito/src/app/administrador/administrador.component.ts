5a678617c98c38120e2a1c41af86e957984d7b4f
Valegg01 <valentinagomezg75@gmail.com>
2025-03-14 22:44:28 -0500

    cambios en el administrador component

--- frontend_cacharrito/src/app/administrador/administrador.component.ts
+++ frontend_cacharrito/src/app/administrador/administrador.component.ts
@@ -1,16 +1,41 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ServicioVehiculoService } from '../servicios/servicio-vehiculo.service';
+import { Vehiculo } from '../Entidades/vehiculo';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-administrador',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule ,FormsModule],
   templateUrl: './administrador.component.html',
   styleUrl: './administrador.component.css'
 })
 export class AdministradorComponent {
 
-  
+  constructor(private servicio:ServicioVehiculoService){}
+
+  tipo!:string;
+  estado!: string;
+
+  vehiculos!: Vehiculo[];
+
+
+  filtrar(){
+
+    this.servicio.filtro_tipo_estado(this.tipo,this.estado).subscribe(dato=>{
+
+      if(dato != false){
+
+        this.vehiculos=dato
+
+      }else{
+        alert("no se encuentran vehiculo registrados")
+      }
+
+    })
+
+  }
 
   ver_alquiler(){
     
