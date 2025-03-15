6697343338bde4eebd0476b412d2a773097d1c64
Valegg01 <valentinagomezg75@gmail.com>
2025-03-14 19:45:02 -0500

    creacion d ela interface del administrador frontend

--- frontend_cacharrito/src/app/administrador/administrador.component.ts
+++ frontend_cacharrito/src/app/administrador/administrador.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-administrador',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './administrador.component.html',
   styleUrl: './administrador.component.css'
 })
 export class AdministradorComponent {
+
+  
+
+  ver_alquiler(){
+    
+    const alq = document.getElementById("alquiler");
+    const veh = document.getElementById("vehiculo");
+
+    alq!.style.display='block';
+    veh!.style.display='none';
+
+  }
+
+  ver_vehiculo(){
+    
+
+    const alq = document.getElementById("alquiler");
+    const veh = document.getElementById("vehiculo");
+
+    alq!.style.display='none';
+    veh!.style.display='block';
+
+  }
 
 }
